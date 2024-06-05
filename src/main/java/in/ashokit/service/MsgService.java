b9e56fe48c85cce77dbc92c10740322864d52e13
AkhilReddyYalla <yallaakhilreddy2020@gmail.com>
2024-06-05 22:45:16 +0530

    updated

---
+++ src/main/java/in/ashokit/service/MsgService.java
@@ -0,0 +1,22 @@
+package in.ashokit.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MsgService {
+
+	private Logger logger = LoggerFactory.getLogger(MsgService.class);
+
+	public String getWelcomeMsg() {
+
+		logger.info("getWelcomeMsg() - started...");
+
+		String msg = "Welcome to Ashok IT";
+
+		logger.info("getWelcomeMsg() - ended....");
+
+		return msg;
+	}
+}
